20b6b15c874309de35a9951ed78913c89130d0d0
Luke Wren <wren6991@gmail.com>
2019-03-06 10:28:56 +0000

    Add RoundRobin, and simple test case for this. Add PrioritySelector with formal spec.

--- nmigen/lib/coding.py
+++ nmigen/lib/coding.py
@@ -1,12 +1,15 @@
 """Encoders and decoders between binary and one-hot representation."""
 
 from .. import *
-
+from enum import Enum
 
 __all__ = [
     "Encoder", "Decoder",
     "PriorityEncoder", "PriorityDecoder",
+    "PrioritySelector",
     "GrayEncoder", "GrayDecoder",
+    "RobinPolicy",
+    "RoundRobinSelector", "RoundRobinEncoder"
 ]
 
 
@@ -84,6 +87,40 @@ class PriorityEncoder:
         m.d.comb += self.n.eq(self.i == 0)
         return m
 
+
+# Exclusive left smear
+def _smear_lx(signal):
+    return Cat(signal[:i].bool() for i in range(signal.nbits))
+
+class PrioritySelector:
+        """Input a bitmap, ``i`` with any number of bits set.
+        Output a bitmap, ``o``, with only the least significant of these set.
+        If ``i`` is all zeroes then so is ``o``.
+
+        Parameters
+        ----------
+        width : int
+            Bit width.
+
+        Attributes
+        ----------
+        i : Signal(width), in
+            Input request bitmap
+        o : Signal(width), out
+            Output grant bitmap
+        """
+        def __init__(self, width):
+            self.i = Signal(width)
+            self.o = Signal(width)
+            self.width = width
+
+        def elaborate(self, platform):
+            m = Module()
+
+            deny = _smear_lx(self.i)
+            m.d.comb += self.o.eq(self.i & ~deny)
+
+            return m
 
 class Decoder:
     """Decode binary to one-hot.
@@ -183,4 +220,131 @@ class GrayDecoder:
         m.d.comb += self.o[-1].eq(self.i[-1])
         for i in reversed(range(self.width - 1)):
             m.d.comb += self.o[i].eq(self.o[i + 1] ^ self.i[i])
+        return m
+
+
+class RobinPolicy(Enum):
+    """
+    RobinPolicy.WITHDRAW:
+        Once granted, a given request remains granted on every cycle
+        until that request is withdrawn, at which point there is a new
+        round of arbitration.
+    RobinPolicy.CE:
+        A new round of arbitration on every cycle where ``en`` is high.
+        The currently-granted request will only be re-granted if
+        there are no other requests.
+    """
+    WITHDRAW = 0
+    CE       = 1
+
+
+class RoundRobinSelector:
+    """Grant each of a set of requests, in turn, in a rotating fashion.
+
+    In each round of arbitration, only requests higher-numbered than
+    the most-recently-granted are considered, with priority to the
+    lowest-numbered of these. If there are no such requests, the
+    lowest-numbered of all requests is chosen.
+
+    Parameters
+    ----------
+    n_reqs : int
+        Number of simultaneous requests
+    policy : RobinPolicy
+        Policy on when to perform a new round of arbitration.
+        See :class:``RobinPolicy``
+
+    Attributes
+    ----------
+    i : Signal(n_reqs)
+        Input request bitmap.
+    o : Signal(n_reqs)
+        Output grant bitmap.
+        There is no register on the path between ``i`` and ``o``;
+        requests can be presented and granted in-cycle.
+    en : Signal(1)
+        Request enable. Only present if ``policy`` is RobinPolicy.CE.
+        A newly-arbitrated grant bitmap is presented on ``o`` on
+        cycles where ``en`` is asserted.
+        Otherwise, ``o`` does not change.
+    """
+    def __init__(self, n_reqs, policy=RobinPolicy.CE):
+        if not isinstance(policy, RobinPolicy):
+            raise TypeError("policy must be a RobinPolicy, not '{!r}'".format(policy))
+        if not isinstance(n_reqs, int) or n_reqs < 1:
+            raise TypeError("n_reqs must be a positive integer, not '{!r}'".format(n_reqs))
+        self.n_reqs = n_reqs
+        self.policy = policy
+        self.i = Signal(n_reqs)
+        self.o = Signal(n_reqs)
+        if policy == RobinPolicy.CE:
+            self.en = Signal()
+
+    def elaborate(self, platform):
+        m = Module()
+
+        grant_prev = Signal(self.n_reqs)
+        req_unwrapped = Cat(self.i & _smear_lx(grant_prev), self.i)
+        psel = m.submodules.psel = PrioritySelector(self.n_reqs * 2)
+        m.d.comb += psel.i.eq(req_unwrapped)
+        grant = psel.o[:self.n_reqs] | psel.o[self.n_reqs:]
+
+        if self.policy == RobinPolicy.WITHDRAW:
+            en = ~((self.i & grant_prev).bool())
+        else:
+            en = self.en
+
+        with m.If(en):
+            m.d.sync += grant_prev.eq(grant)
+            m.d.comb += self.o.eq(grant)
+        with m.Else():
+            m.d.comb += self.o.eq(grant_prev)
+
+        return m
+
+class RoundRobinEncoder:
+    """Identical to :class:`RoundRobinSelector`, except its output is encoded.
+
+    Attributes
+    ----------
+    i : Signal(n_reqs), in
+        Input request bitmap.
+    o : Signal(max=max(2, n_reqs)), out
+        Output grant index.
+        There is no register on the path between ``i`` and ``o``;
+        requests can be presented and granted in-cycle.
+    n : Signal(1), out
+        1 if no request is currently granted.
+    en : Signal(1), in
+        Request enable. Only present if ``policy`` is CE.
+        A newly-arbitrated grant bitmap is presented on ``o`` on
+        cycles where ``en`` is asserted.
+        Otherwise, ``o`` does not change.
+    """
+    def __init__(self, n_reqs, policy=RobinPolicy.CE):
+        self.n_reqs = n_reqs
+        self.policy = policy
+        self.i = Signal(n_reqs)
+        self.o = Signal(max=max(2,n_reqs))
+        self.n = Signal()
+        if policy == RobinPolicy.CE:
+            self.en = Signal()
+
+    def elaborate(self, platform):
+        m = Module()
+
+        robin = m.submodules.robin = \
+            RoundRobinSelector(self.n_reqs, self.policy)
+        enc = m.submodules.enc = Encoder(self.n_reqs)
+
+        if self.policy == RobinPolicy.CE:
+            m.d.comb += robin.en.eq(self.en)
+
+        m.d.comb += [
+            robin.i.eq(self.i),
+            enc.i.eq(robin.o),
+            self.o.eq(enc.o),
+            self.n.eq(enc.n)
+        ]
+
         return m
--- nmigen/test/test_lib_coding.py
+++ nmigen/test/test_lib_coding.py
@@ -1,5 +1,6 @@
 from .tools import *
 from ..hdl.ast import *
+from ..hdl.cd import *
 from ..hdl.dsl import *
 from ..back.pysim import *
 from ..lib.coding import *
@@ -82,6 +83,35 @@ class DecoderTestCase(FHDLTestCase):
             sim.run()
 
 
+class PrioritySelectorSpec:
+    def __init__(self, width):
+        self.width = width
+
+    def elaborate(self, platform):
+        m = Module()
+        sel = m.submodules.sel = PrioritySelector(self.width)
+
+        m.d.comb += Assert(sel.i.bool() == sel.o.bool())
+        if_elif = m.If
+        for n in range(self.width):
+            with if_elif(sel.i[n]):
+                m.d.comb += Assert(sel.o[n])
+            if_elif = m.Elif
+        return m
+
+
+class PrioritySelectorTest(FHDLTestCase):
+    def test_formal1(self):
+        self.check_formal(1)
+
+    def test_formal10(self):
+        self.check_formal(10)
+
+    def check_formal(self, width):
+        spec = PrioritySelectorSpec(width)
+        self.assertFormal(spec, mode="prove")
+
+
 class ReversibleSpec:
     def __init__(self, encoder_cls, decoder_cls, args):
         self.encoder_cls = encoder_cls
@@ -124,3 +154,88 @@ class GrayCoderTestCase(FHDLTestCase):
     def test_distance(self):
         spec = HammingDistanceSpec(distance=1, encoder_cls=GrayEncoder, args=(16,))
         self.assertFormal(spec, mode="prove")
+
+
+class RoundRobinSelectorSimpleCase(FHDLTestCase):
+    def test_paramcheck(self):
+        with self.assertRaises(TypeError):
+            r = RoundRobinSelector(0)
+        with self.assertRaises(TypeError):
+            r = RoundRobinSelector(1, 0)
+        with self.assertRaises(TypeError):
+            r = RoundRobinSelector(1, "WITHDRAW")
+        r = RoundRobinSelector(1, RobinPolicy.WITHDRAW)
+
+    def test_walk_withdraw_2(self):
+        self.check_walk_withdraw(2)
+
+    def test_walk_withdraw_10(self):
+        self.check_walk_withdraw(10)
+
+    def test_walk_enable_2(self):
+        self.check_walk_enable(2)
+
+    def test_walk_enable_10(self):
+        self.check_walk_enable(10)
+
+    def check_tick(self, rr, bits):
+        yield Delay(1e-8)
+        self.assertEqual((yield rr.o), bits)
+        yield Tick()
+
+    def check_walk_withdraw(self, width):
+        m = Module()
+        m.domains += ClockDomain("sync")
+        rr = m.submodules.rr = RoundRobinSelector(width, RobinPolicy.WITHDRAW)
+        with Simulator(m, vcd_file = open("test.vcd", "w")) as sim:
+            sim.add_clock(1e-6)
+            def process():
+                yield rr.i.eq(0)
+                yield Tick()
+                yield Tick()
+                for i in range(width):
+                    j = (i + 1) % width
+                    yield rr.i[i].eq(1)
+                    yield from self.check_tick(rr, 1 << i)
+                    yield rr.i[j].eq(1)
+                    yield from self.check_tick(rr, 1 << i)
+                    yield rr.i[i].eq(0)
+                    yield from self.check_tick(rr, 1 << j)
+                    yield rr.i[j].eq(0)
+                    yield from self.check_tick(rr, 0)
+            sim.add_process(process)
+            sim.run()
+
+    def check_walk_enable(self, width):
+        m = Module()
+        m.domains += ClockDomain("sync")
+        rr = m.submodules.rr = RoundRobinSelector(width, RobinPolicy.CE)
+        with Simulator(m, vcd_file = open("test.vcd", "w")) as sim:
+            sim.add_clock(1e-6)
+            def process():
+                yield rr.i.eq(0)
+                yield rr.en.eq(0)
+                yield Tick()
+                for i in range(width):
+                    j = (i + 1) % width
+                    yield rr.i[i].eq(1)
+                    yield rr.en.eq(1)
+                    yield from self.check_tick(rr, 1 << i)
+                    yield from self.check_tick(rr, 1 << i)
+                    yield rr.i[j].eq(1)
+                    yield rr.en.eq(0)
+                    yield from self.check_tick(rr, 1 << i)
+                    yield rr.en.eq(1)
+                    yield from self.check_tick(rr, 1 << j)
+                    yield from self.check_tick(rr, 1 << i)
+                    yield from self.check_tick(rr, 1 << j)
+                    yield rr.i[i].eq(0)
+                    yield from self.check_tick(rr, 1 << j)
+                    yield from self.check_tick(rr, 1 << j)
+                    yield rr.i[j].eq(0)
+                    yield rr.en.eq(0)
+                    yield from self.check_tick(rr, 1 << j)
+                    yield rr.en.eq(1)
+                    yield from self.check_tick(rr, 0)
+            sim.add_process(process)
+            sim.run()
